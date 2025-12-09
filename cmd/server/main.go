29e5c9399641b0d2aefd1f7256ca371b84df87c4
Vova4o <gavrilenko2012@gmail.com>
2025-12-09 14:47:11 +0300

    add: token log

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -21,11 +21,13 @@ func main() {
 	if token == "" {
 		log.Fatal("TELEGRAM_BOT_TOKEN is not set")
 	}
+	log.Printf("TELEGRAM_BOT_TOKEN=%s", token)
 
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
+	log.Printf("PORT=%s", port)
 
 	// Initialize Telegram bot client.
 	botClient := bot.NewBotClient(token)
